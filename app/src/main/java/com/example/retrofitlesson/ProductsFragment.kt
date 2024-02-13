c349205f626e24aba88410d6617a1d7436b2740d
fffgladevg <mrmrmr1487@gmail.com>
2024-02-13 22:24:08 +0300

    viewModel plus xml plus fragment

---
+++ app/src/main/java/com/example/retrofitlesson/ProductsFragment.kt
@@ -0,0 +1,34 @@
+package com.example.retrofitlesson
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.retrofitlesson.databinding.FragmentProductsBinding
+
+
+class ProductsFragment : Fragment() {
+
+    private lateinit var binding: FragmentProductsBinding
+    private val viewModel: LoginViewModel by activityViewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentProductsBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.token.observe(viewLifecycleOwner) {
+            token ->
+        }
+
+    }
+}
